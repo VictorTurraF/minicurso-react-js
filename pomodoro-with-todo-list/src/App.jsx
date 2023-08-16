ef77ef84749d2a5cde85fa5f5cac7c23e22c0e52
Victor Turra <victor.florencio@univem.edu.br>
2023-08-16 17:32:54 -0300

    chore: lint code

--- pomodoro-with-todo-list/src/App.jsx
+++ pomodoro-with-todo-list/src/App.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-import { styled } from "@stitches/react"
-import { rebootCss } from "./styles/reboot"
+import React, { useEffect, useState } from "react";
+import { styled } from "@stitches/react";
+import { rebootCss } from "./styles/reboot";
 import { durationMap, pomodoroSteps, settings } from "./configs/settings";
 import { configsTable, taskTable } from "./configs/storages";
 import TimerContainer from "./components/TimerContainer";
 import TaskContainer from "./components/TaskContainer";
 import useList from "./hooks/useList";
 
-const AppGrid = styled('div', {
-  display: 'grid',
+const AppGrid = styled("div", {
+  display: "grid",
   gridTemplateColumns: "4fr 6fr",
   gridGap: "2rem",
   maxWidth: "1000px",
   margin: "3rem auto",
-})
+});
 
 rebootCss();
 
@@ -23,105 +23,108 @@ function App() {
     setList: setTasks,
     addItem: addTask,
     removeItemByField: removeTaskBy,
-    updateItemByField: updateTaskBy
-  } = useList([])
+    updateItemByField: updateTaskBy,
+  } = useList([]);
 
   const [selectedTaskId, setSelectedTaskId] = useState();
-
-  const [remainingSeconds, setRemainingSeconds] = useState(settings.pomodoroDurationInSeconds);
-  const [currentStep, setCurrentStep] = useState(0)
-  const [isTimerRunning, setIsTimerRunning] = useState(false)
-
-  const stepPosition = currentStep % pomodoroSteps.length
+  const [remainingSeconds, setRemainingSeconds] = useState(
+    settings.pomodoroDurationInSeconds
+  );
+  const [currentStep, setCurrentStep] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const stepPosition = currentStep % pomodoroSteps.length;
 
   function handleTimerToggle() {
-    setIsTimerRunning(prev => !prev)
+    setIsTimerRunning((prev) => !prev);
   }
 
   function getCurrentStepDuration() {
-    return durationMap.get(pomodoroSteps[stepPosition])
+    return durationMap.get(pomodoroSteps[stepPosition]);
   }
 
   useEffect(() => {
     let interval;
 
     if (isTimerRunning) {
-      let remainingTime = remainingSeconds
+      let remainingTime = remainingSeconds;
 
       interval = setInterval(() => {
-
         if (remainingTime <= 0) {
-          advanceStep()
-          clearInterval(interval)
-
+          incrementPomodoroAndAdvanceStep();
+          clearInterval(interval);
         } else {
-          setRemainingSeconds(prev => prev - 1)
+          setRemainingSeconds((prev) => prev - 1);
           remainingTime -= 1;
         }
+      }, 1000);
 
-      }, 300);
-
-    } else {
-      clearInterval(interval)
+      return () => {
+        clearInterval(interval);
+      };
     }
-
-    return () => {
-      clearInterval(interval);
-    };
   }, [isTimerRunning, stepPosition]);
 
-  const advanceStep = () => {
+  const incrementPomodoroAndAdvanceStep = () => {
     if (pomodoroSteps[stepPosition] === "pomodoro") {
-      const selectedTask = tasks.find(task => task.id === selectedTaskId);
-      updateTaskBy('id', selectedTaskId, { 
-        ...selectedTask, 
-        actPomodoros: selectedTask.actPomodoros + 1 
-      })
+      const selectedTask = tasks.find((task) => task.id === selectedTaskId);
+      if (selectedTask) {
+        incrementTaskActPomodoros(selectedTask)
+      }
     }
 
-    const nextStep = pomodoroSteps[(currentStep + 1) % 8]
-    setRemainingSeconds(durationMap.get(nextStep))
-    setCurrentStep(prev => prev + 1)
+    advanceStep()
+  };
 
+  const incrementTaskActPomodoros = (task) => {
+    updateTaskBy("id", task.id, {
+      ...task,
+      actPomodoros: task.actPomodoros + 1,
+    });
+  }
+
+  const advanceStep = () => {
+    setRemainingSeconds(durationMap.get(pomodoroSteps[(currentStep + 1) % 8]));
+    setCurrentStep((prev) => prev + 1);
   }
 
   const handleTaksFormSubmit = ({ form: task }) => {
-    if (!!task.description && !!task.estimatedPomodoros) {
-      addTask(task);
-      taskTable.setItem(task.id, task)
+    if (task.description && task.estimatedPomodoros) {
+      const taskId = addTask(task);
+      taskTable.setItem(taskId, task);
     }
-  }
+  };
 
   const handleFinishStepClick = () => {
-    advanceStep()
-  }
+    incrementPomodoroAndAdvanceStep();
+  };
 
   const handleTaskDeleteClick = ({ taskId }) => {
-    removeTaskBy('id', taskId);
-    taskTable.removeItem(taskId)
-  }
+    removeTaskBy("id", taskId);
+    taskTable.removeItem(taskId);
+  };
 
   const handleTaskSelectClick = ({ taskId }) => {
-    setSelectedTaskId(taskId)
-    configsTable.setItem('selectedTaskId', taskId)
-  }
+    setSelectedTaskId(taskId);
+    configsTable.setItem("selectedTaskId", taskId);
+  };
 
   useEffect(() => {
-    const tasks = []
+    const tasks = [];
+
     taskTable
-      .iterate((value, key) => {
-        tasks.push(value)
+      .iterate((value) => {
+        tasks.push(value);
       })
       .then(() => {
         setTasks(tasks);
       });
-  }, [])
+  }, []);
 
   useEffect(() => {
     configsTable
-      .getItem('selectedTaskId')
-      .then(value => setSelectedTaskId(value))
-  }, [])
+      .getItem("selectedTaskId")
+      .then((value) => setSelectedTaskId(value));
+  }, []);
 
   return (
     <AppGrid>
@@ -144,4 +147,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
